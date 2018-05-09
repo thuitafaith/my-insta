cf2f2b61ad60c765984229985b66b784f4c7c5ca
thuitafaith <thuitamuthoni15@gmail.com>
2018-05-09 14:33:35 +0300

    renamed project name

--- thegram/models.py
+++ thegram/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_link = models.ImageField(upload_to ='pics/')
+    name = models.CharField(max_length=100)
+    caption = models.CharField(max_length=100)
+    comments = models.TextField()
