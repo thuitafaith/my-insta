475eb987e5fb6241ea8af821c00918deb047b839
thuitafaith <thuitamuthoni15@gmail.com>
2018-05-09 17:13:07 +0300

    create test save for profile model

--- gram/models.py
+++ gram/models.py
@@ -14,6 +14,14 @@ class Image(models.Model):
     @classmethod
     def update_caption(cls,id,caption):
         cls.objects.filter(id = id).update(caption=caption)
+    @classmethod
+    def get_image_by_id(cls,id):
+        imge = cls.objects.filter(id=id).all()
+        return imge
+
 class Profile(models.Model):
     profile_photo = models.ImageField(upload_to='pics/')
     Bio = models.TextField()
+
+    def save_profile(self):
+        self.save()
--- gram/tests.py
+++ gram/tests.py
@@ -24,3 +24,21 @@ class ImageTestClass(TestCase):
         self.new_image.update_caption(self.new_image.id,'live life')
         image = Image.objects.filter(caption='live life').all()
         self.assertTrue(len(image)==1)
+    #get image by id test method
+    def test_get_image_by_id(self):
+        find_img = self.new_image.get_image_by_id(self.new_image.id)
+        img = Image.objects.filter(id = self.new_image.id)
+        self.assertTrue(find_img,img)
+    def tearDown(self):
+        Image.objects.all().delete()
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.profile=Profile(profile_photo='pics/image.jpeg',Bio ='i am me')
+        self.profile.save()
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+    def test_save_method(self):
+        self.profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
