3a4dcd165b235f23d13bbd9ebe72442e0d034f8b
thuitafaith <thuitamuthoni15@gmail.com>
2018-05-10 11:48:01 +0300

    install django registration

--- gram/models.py
+++ gram/models.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 class Image(models.Model):
     image_link = models.ImageField(upload_to ='pics/')
     name = models.CharField(max_length=100)
-    caption = models.CharField(max_length=100)
-    comments = models.TextField()
+    caption = models.CharField(max_length=60,blank=True,null=True)
+    likes = models.IntegerField()
+    image_comments = models.ForeignKey('Comment',null=True)
     profile = models.ForeignKey('Profile',null=True)
 
     def save_image(self):
@@ -28,3 +29,13 @@ class Profile(models.Model):
         self.save()
     def delete_profile(self):
         self.delete()
+
+class Like(models.Model):
+    user = models.ForeignKey(User)
+    image = models.ForeignKey(Image)
+    created = models.DateTimeField(auto_now_add =True)
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=500)
+    user = models.ForeignKey(User)
+    image = models.ForeignKey(Image)
