c1ae64f229fb43ef0ca8ce92d5628e9e754fb6e9
thuitafaith <thuitamuthoni15@gmail.com>
2018-05-10 14:52:51 +0300

    create new file registration_html

--- gram/models.py
+++ gram/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+
 # Create your models here.
 class Image(models.Model):
     """
     Initializing image model
-    
+
     """
     image_link = models.ImageField(upload_to ='pics/')
     name = models.CharField(max_length=100)
     caption = models.CharField(max_length=60,blank=True,null=True)
-    likes = models.IntegerField()
-    image_comments = models.ForeignKey('Comment',null=True)
+    likes = models. ManyToManyField(Like)
+    image_comments = models.ManyToManyField(Comment)
     profile = models.ForeignKey('Profile',null=True)
 
     def save_image(self):
@@ -25,21 +28,18 @@ class Image(models.Model):
         imge = cls.objects.filter(id=id).all()
         return imge
 
+    def __str__(self):
+
+
 class Profile(models.Model):
     profile_photo = models.ImageField(upload_to='pics/')
+    avatar_thumbnail =ImageSpecField(source='profile_photo',
+                                     processors=[ResizeToFill(100, 50)],
+                                     format='JPEG',
+                                     options={'quality': 60})
     Bio = models.TextField()
     user = models.ForeignKey(User)
     def save_profile(self):
         self.save()
     def delete_profile(self):
         self.delete()
-
-class Like(models.Model):
-    user = models.ForeignKey(User)
-    image = models.ForeignKey(Image)
-    created = models.DateTimeField(auto_now_add =True)
-
-class Comment(models.Model):
-    comment = models.CharField(max_length=500)
-    user = models.ForeignKey(User)
-    image = models.ForeignKey(Image)
--- heyinsta/settings.py
+++ heyinsta/settings.py
@@ -116,7 +116,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+ACCOUNT_ACTIVATION_DAYS = 7 # one-week activation window
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
