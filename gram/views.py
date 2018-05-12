b6dc16dce6e23f87acb5de80ebaaef271164249e
thuitafaith <thuitamuthoni15@gmail.com>
2018-05-13 01:42:24 +0300

    install python-decouple to import config

--- gram/views.py
+++ gram/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login,authenticate
-from django.contrib.auth.forms import UserCreationForm
+from gram.forms import SignUpForm
 
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -14,5 +14,5 @@ def signup(request):
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'registration/signup.html', {'form':form})
