4c29237107541544eb2200fa451d979b4f2949f9
thuitafaith <thuitamuthoni15@gmail.com>
2018-05-13 00:26:20 +0300

    create new file base.html

--- gram/views.py
+++ gram/views.py
@@ -15,4 +15,4 @@ def signup(request):
             return redirect('home')
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html', {'form':form})
+    return render(request, 'registration/signup.html', {'form':form})
