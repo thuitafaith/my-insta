cedb1071da095e535208ea08973c33e7fa92e098
thuitafaith <thuitamuthoni15@gmail.com>
2018-05-13 00:00:45 +0300

    rename registration.html to signup.html

--- heyinsta/urls.py
+++ heyinsta/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from heyinsta.gram import views as gram_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.hmac.urls'))
+    url(r'^signup/$', gram_views.signup, name ='signup')
+
+
 ]
